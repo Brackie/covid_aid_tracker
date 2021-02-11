733e02e10cc4cb94342100c6aab2b2693bf5eeaa
Pancakem <pancakesdeath@protonmail.com>
2021-02-11 12:05:45 +0300

    Add barchart view function

--- frontend/src/elm/Visualize/Barchart.elm
+++ frontend/src/elm/Visualize/Barchart.elm
@@ -30,8 +30,6 @@ yAxis : Svg msg
 yAxis =
     Axis.left [ Axis.tickCount 5 ] yScale
 
--- bar chart
-
 column :  BandScale a -> (a,  Float) -> (a -> String )-> Svg msg
 column scale (xval, yval) toStr =
     g [ class [ "column"]]
@@ -50,3 +48,19 @@ column scale (xval, yval) toStr =
             [ text <| String.fromFloat yval ]
         ]
 
+view : List (a, Float) -> Svg msg
+view model =
+    svg [viewBox 0 0 w  h]
+        [style [] [ text """
+                          .column rect { fill: rgba(118, 214, 78, 0.8);}
+                          .column text { display: none; }
+                          .column:hover rect { fill: rgb(118, 214, 78); }
+                          .column:hover text { display: inline; }
+                          """]
+             , g [transform [Translate (padding - 1) (h - padding)]]
+             [ xAxis model]
+             , g [transform [Translate (padding - 1) padding]]
+             [yAxis]
+             , g [transform [Translate padding padding ], class [
+                      "series"]] <| List.map (column (xScale model)) model
+        ]
