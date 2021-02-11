f49d109a8952a56d81463197232b37f21ac0fa1d
JerryNyoike <jngugi31@gmail.com>
2021-02-11 14:30:42 +0300

    Start module for fetching and working with funding data

---
+++ frontend/src/elm/Funding.elm
@@ -0,0 +1,61 @@
+module Funding exposing (..)
+
+import Http
+import Iso8601
+import Json.Decode as Decode exposing (Decoder, Value, int, list, map3, null, oneOf, string, succeed)
+import Json.Decode.Pipeline exposing (required)
+import Time exposing (Posix)
+
+
+type alias Funding =
+    { amount : Int
+    , source : String
+    , recepient : String
+    , counties : List String
+    , date : Posix
+    }
+
+
+type Msg
+    = GotFundingData (Result Http.Error Funding)
+
+
+getFundingData : Cmd Msg
+getFundingData =
+    Http.get
+        { url = "https://actionfortransparency.org/wp-content/plugins/a4t-covid-19/assets/funding.json"
+        , expect = Http.expectJson GotFundingData fundingDecoder
+        }
+
+
+
+-- helpers
+
+
+inKindDonation : Funding -> Bool
+inKindDonation funding =
+    funding.amount == 0
+
+
+
+-- decoders
+
+
+amountDecoder : Decoder Int
+amountDecoder =
+    oneOf [ int, succeed 0 ]
+
+
+countiesDecoder : Decoder (List String)
+countiesDecoder =
+    list string
+
+
+fundingDecoder : Decoder Funding
+fundingDecoder =
+    Decode.succeed Funding
+        |> required "amount" amountDecoder
+        |> required "donor" string
+        |> required "recepient" string
+        |> required "counties" countiesDecoder
+        |> required "period" Iso8601.decoder
